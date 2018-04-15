0e6d6d466b5f864df6a7e521dd8c59bce06470d5
Yoana <yoana19st@gmail.com>
2018-04-15 23:23:13 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.multicalc.multicalc;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    private Spinner spinner1;
+    private Button button;
+    double t1;
+    double t2;
+    double t3;
+    double result;
+
+
+}
+
+
+
+
